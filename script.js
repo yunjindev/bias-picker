da9dc74210f638a13c43a24acccc1f9b28175c86
Hunter <hunterweisenbach@me.com>
2026-01-27 23:01:10 -0500

    redesigned js file

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 const testBtn = document.getElementById("myBtn");
-const idolLists = document.querySelectorAll(".idol-list li");
+const idolLists = document.querySelectorAll(".idol-list");
+const idolListsLi = document.querySelectorAll(".idol-list li");
 let selectedIdol = "";
 
 
+
+// Button to show idols
 testBtn.addEventListener("click", () => {
     idolLists.forEach(idolList => {
-        idolList.style.visibility = "visible";
+        idolList.style.display = "block";
     });
 });
 
-idolLists.forEach(idolList => {
+// Button to hide idols
+removeBtn.addEventListener("click", () => {
+    idolLists.forEach(idolList => {
+        idolList.style.display = "none";
+    });
+});
+
+
+idolListsLi.forEach(idolList => {
     idolList.addEventListener("click", () => {
         selectedIdol = idolList.textContent;
+        
         console.log("Selected Idol:", selectedIdol);
         let newh1 = document.createElement("h1");
         newh1.className = "selected-bias";
         newh1.textContent = "Your selected bias is: " + selectedIdol;
         document.getElementById("selected-bias-container").appendChild(newh1);
+    
+            
+
     });
 });
+
+
 
