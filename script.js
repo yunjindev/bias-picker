5bc9dcc831308418ce8a72e4c6fdd7f7f67104b2
Hunter <hunterweisenbach@me.com>
2026-01-27 19:31:57 -0500

    design commit subject to change

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const testBtn = document.getElementById("myBtn");
-const idolList = document.querySelector(".idol-list.lesserafim");
+const idolLists = document.querySelectorAll(".idol-list li");
+let selectedIdol = "";
+
 
 testBtn.addEventListener("click", () => {
-    idolList.style.visibility = "visible";
+    idolLists.forEach(idolList => {
+        idolList.style.visibility = "visible";
+    });
 });
+
+idolLists.forEach(idolList => {
+    idolList.addEventListener("click", () => {
+        selectedIdol = idolList.textContent;
+        console.log("Selected Idol:", selectedIdol);
+        let newh1 = document.createElement("h1");
+        newh1.className = "selected-bias";
+        newh1.textContent = "Your selected bias is: " + selectedIdol;
+        document.getElementById("selected-bias-container").appendChild(newh1);
+    });
+});
+
